2ac911dbbe90bec66ec413f16e0a94a9f562ab20
Adam <Madotsuki@myself.com>
2012-07-08 11:50:41 -0500

    WMIQuery Update
    Added the function WMIQuery to make the calls seem neater. Also cuts
    back a lot on size and makes it more readable.

--- winspec.py
+++ winspec.py
@@ -1,73 +1,65 @@
-import os, platform, datetime
+import os
+import platform
+import datetime
 
 from colorama import init, Style, Fore
 from wmi import WMI
 from win32com.client import GetObject
 
-def Memory():
+def WMIQuery(value, mgmtclass):
     w = WMI('.')
-    result = w.query("SELECT FreePhysicalMemory FROM Win32_OperatingSystem")
-    Free = int(result[0].FreePhysicalMemory) / 1024
-    result = w.query("SELECT TotalVisibleMemorySize FROM Win32_OperatingSystem")
-    Total = int(result[0].TotalVisibleMemorySize) / 1024
+    result = w.query("SELECT " + value + " FROM " + mgmtclass)
+    return eval('result[0].' + value)
+
+def Memory():
+    Free = int(WMIQuery('FreePhysicalMemory', 'Win32_OperatingSystem')) / 1024
+    Total = int(WMIQuery('TotalVisibleMemorySize', 'Win32_OperatingSystem')) / 1024
     Percent = Free / float(Total)
+
     returnVal = ""
     if Percent <= .1:
-		returnVal = Fore.RED
+        returnVal = Fore.RED
     elif Percent <= .5:
-		returnVal = Fore.YELLOW
+        returnVal = Fore.YELLOW
     else:
-		returnVal = Fore.GREEN
+        returnVal = Fore.GREEN
 
     returnVal += str(Free) + Fore.WHITE + "MB/"
     returnVal += str(Total) + "MB"
     return returnVal
 
 def Resolution():
-    w = WMI('.')
-    result = w.query("SELECT ScreenWidth FROM Win32_DesktopMonitor")
-    returnVal = str(result[0].ScreenWidth) + 'x'
-    result = w.query("SELECT ScreenHeight FROM Win32_DesktopMonitor")
-    returnVal += str(result[0].ScreenHeight)
+    returnVal = str(WMIQuery('ScreenWidth', 'Win32_DesktopMonitor')) + 'x'
+    returnVal += str(WMIQuery('ScreenHeight', 'Win32_DesktopMonitor'))
     return returnVal
 
 def Harddrive():
-    w = WMI('.')
-    result = w.query("SELECT DeviceID FROM Win32_LogicalDisk")
-
-    result = w.query("SELECT FreeSpace FROM Win32_LogicalDisk")
-    Free = int(result[0].FreeSpace) / 1024**3
-    result = w.query("SELECT Size FROM Win32_LogicalDisk")
-    Size = int(result[0].Size) / 1024**3
+    Free = int(WMIQuery('FreeSpace', 'Win32_LogicalDisk')) / 1024**3
+    Size = int(WMIQuery('Size', 'Win32_LogicalDisk')) / 1024**3
     Percent = Free / float(Size)
+
     returnVal = Fore.CYAN + 'Disk: '
     if Percent <= .1:
-		returnVal += Fore.RED
+        returnVal += Fore.RED
     elif Percent <= .5:
-		returnVal += Fore.YELLOW
+        returnVal += Fore.YELLOW
     else:
-		returnVal += Fore.GREEN
+        returnVal += Fore.GREEN
 
     returnVal += str(Free) + Fore.WHITE + "GB/" + str(Size) + "GB"
     return returnVal
 
 def CPU():
-    w = WMI('.')
-    result = w.query("SELECT Name FROM Win32_Processor")
-    returnVal = str(result[0].Name).replace("(TM)", "")
+    returnVal = str(WMIQuery('Name', 'Win32_Processor'))
+    returnVal = returnVal.replace("(TM)", "")
     returnVal = returnVal.replace("CPU ", "")
     return returnVal
 
 def GPU():
-    w = WMI('.')
-    result = w.query("SELECT Name FROM Win32_VideoController")
-    returnVal = str(result[0].Name)
-    return returnVal
+    return str(WMIQuery('Name', 'Win32_VideoController'))
     
 def UpTime():
-    w = WMI('.')
-    result = w.query("SELECT LastBootUpTime FROM Win32_OperatingSystem")
-    lastBoot = str(result[0].LastBootUpTime)
+    lastBoot = str(WMIQuery('LastBootUpTime', 'Win32_OperatingSystem'))
     returnVal = datetime.datetime.now() - datetime.datetime.strptime(lastBoot[0:13],'%Y%m%d%H%M%S')
     return str(returnVal).split('.')[0]
 
@@ -77,14 +69,14 @@ def Processes():
     return str(len(procs))
     
 def SetPosition(width, height):
-	return '\033[' + str(height) + ';' + str(width) + 'H'
+    return '\033[' + str(height) + ';' + str(width) + 'H'
 
 init(autoreset=True)
 
 winLogo = "\n\n\033[31m          ,,,,,,,,,\n\
         \033[31mII?++?II7$$$:,                 \n\
        \033[31m$I??++?I77$ZZOO                  \n\
-       \033[31m7I?++??I77$ZZO8 \033[32m &,           ,, \n\
+       \033[31m7I?++??I77$ZZO8 \033[32m &,           ,: \n\
       \033[31m=I??++?II7$$ZZO \033[32m $8OOZ$:,  ,I77$O \n\
       \033[31m7I?++??I7$$ZZOO \033[32m 8OOZZ$$7III77$ZD \n\
      \033[31m+I??++?II7$$ZOO~\033[32m $8OOZZ$77III7$$Z  \n\
@@ -114,8 +106,8 @@ output = SetPosition(41, 4) + Username
 output += Fore.WHITE + '@'
 output += Computername
 output += SetPosition(41, 6) + 'OS: ' + Fore.WHITE + platform.system() + ' ' + platform.release();
-output += SetPosition(41, 7) + Fore.CYAN + "Memory: " + Fore.WHITE + Memory()
-output += SetPosition(41, 8) + Fore.CYAN + "Resolution: " + Fore.WHITE + Resolution()
+output += SetPosition(41, 7) + Fore.CYAN + "Resolution: " + Fore.WHITE + Resolution()
+output += SetPosition(41, 8) + Fore.CYAN + "Memory: " + Fore.WHITE + Memory()
 output += SetPosition(41, 9) + Harddrive();
 output += SetPosition(41, 10) + Fore.CYAN + "CPU: " + Fore.WHITE + CPU();
 output += SetPosition(41, 11) + Fore.CYAN + "GPU: " + Fore.WHITE + GPU();
